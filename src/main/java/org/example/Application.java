b55df42d7c683d059f79dcab9ac819087b070942
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-06-27 16:48:13 +0200

    Proviamo a inserire nel main

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Collezione collezione = new Collezione();
+        Scanner print = new Scanner(System.in);
+        boolean vediamo = true;
+        while (vediamo) {
+            System.out.println("Scegli: ");
+            System.out.println("1: Aggiungi gioco");
+            System.out.println("2: Cerca per ID");
+            System.out.println("3: Cerca per prezzo");
+            System.out.println("4: Rimuovi gioco");
+            System.out.println("0: Esci");
+            String scegli = print.nextLine();
+
+            switch (scegli) {
+                case "1":
+                    System.out.println("ID: ");
+                    int IdGioco = Integer.parseInt(print.nextLine());
+                    System.out.println("Titolo: ");
+                    String Titolo = print.nextLine();
+                    System.out.println("Anno: ");
+                    int AnnoPubblicazione = Integer.parseInt(print.nextLine());
+            }
+
+        }
     }
 }
--- src/main/java/org/example/Collezione.java
+++ src/main/java/org/example/Collezione.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Collezione {
     private Map<Integer, Giochi> giochi = new HashMap<>();
@@ -14,9 +16,20 @@ public class Collezione {
         giochi.put(gioco.getIdGioco(), gioco);
         return true;
     }
+
+    //Rimuovi gioco
+    public boolean rimuoviGioco(int id) {
+        return giochi.remove(id) != null;
+    }
+
     //Cerca Id
 
     public Optional<Giochi> cercaPerId(int id) {
         return Optional.ofNullable(giochi.get(id));
+    }
+
+    //Cerca per prezzo
+    public List<Giochi> cercaPerPrezzo(double maxPrezzo) {
+        return giochi.values().stream().filter(g -> g.getPrezzo() < maxPrezzo).collect(Collectors.toList());
     }
 }
