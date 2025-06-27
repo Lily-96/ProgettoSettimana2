68d82a49769b138b322bc27ebd8fda66ee566b1d
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-06-27 14:10:18 +0200

    risolta duplicazione

--- src/main/java/org/example/Videogiochi.java
+++ src/main/java/org/example/Videogiochi.java
@@ -1,21 +1,17 @@
 package org.example;
 
-@Override
-public String toString() {
-    return "Videogiochi{" +
-            "Piattaforma='" + Piattaforma + '\'' +
-            ", DurataDiGioco=" + DurataDiGioco +
-            ", genere=" + genere +
-            '}';
-}
 
 public class Videogiochi extends Giochi {
     private String Piattaforma;
     private int DurataDiGioco;
     private Genere genere;
 
-    public Videogiochi(int IdGiochi, String Titolo, int AnnoPubblicazione, double Prezzo, String Piattaforma, int DurataDiGioco, Genere genere) {
+    public Videogiochi(int IdGiochi, String Titolo, int AnnoPubblicazione, double Prezzo) {
         super(IdGiochi, Titolo, AnnoPubblicazione, Prezzo);
+    }
+
+    public void VideoGiochi(int IdGiochi, String Titolo, int AnnoPubblicazione, double Prezzo, String Piattaforma, int DurataDiGioco, Genere genere) {
+
         this.Piattaforma = Piattaforma;
         this.DurataDiGioco = DurataDiGioco;
         this.genere = genere;
@@ -44,6 +40,15 @@ public class Videogiochi extends Giochi {
     public void setGenere(Genere genere) {
         this.genere = genere;
     }
-}
 
+    @Override
+    public String toString() {
+        return "Videogiochi{" +
+                "Piattaforma='" + Piattaforma + '\'' +
+                ", DurataDiGioco=" + DurataDiGioco +
+                ", genere=" + genere +
+                '}';
+    }
+
+}
 
